3cdde7500af9f365522e2e2b2309b5de815c9d16
Gray <grayzprogrammer@gmail.com>
2022-08-15 10:29:05 -0500

    Initial Commit

---
+++ AdMobAndSPM/AdMobAndSPMApp.swift
@@ -0,0 +1,17 @@
+//
+//  AdMobAndSPMApp.swift
+//  AdMobAndSPM
+//
+//  Created by G Zhen on 8/15/22.
+//
+
+import SwiftUI
+
+@main
+struct AdMobAndSPMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
